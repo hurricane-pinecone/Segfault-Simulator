547baaefa448ca7a9da6c3df68fd3479bf6bbd49
James Couch <james.couch@alteryx.com>
2026-05-15 01:08:35 +1000

    Fix web canvas size

--- engine/include/engine/systems/isometricRenderSystem.h
+++ engine/include/engine/systems/isometricRenderSystem.h
@@ -7,7 +7,6 @@
 #include "glm/glm/ext/vector_float2.hpp"
 #include "glm/glm/ext/vector_float3.hpp"
 #include "glm/glm/ext/vector_int2.hpp"
-#include <cstdint>
 
 #ifdef __EMSCRIPTEN__
   #include <GLES3/gl3.h>
